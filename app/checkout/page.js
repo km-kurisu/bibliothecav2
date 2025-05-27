b58e00a2062de0c94379dab5a0a4c9d21e567655
km-kurisu <kamleshkmistry33@gmail.com>
2025-05-28 01:20:36 +0530

    checkout

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,4 +1,6 @@
 'use client'
+export const dynamic = 'force-dynamic';
+
 import { useCart } from '../lib/cart'
 import { useAuth } from '@/context/AuthContext'
 import { createClient } from '@/utils/supabase/client'
