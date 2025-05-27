5b874be6ce3c0435c82fe57d6e31663657818ca4
km-kurisu <kamleshkmistry33@gmail.com>
2025-05-28 01:10:08 +0530

    layout

---
+++ app/admin/layout.js
@@ -0,0 +1,9 @@
+'use client'
+
+export const dynamic = 'force-dynamic';
+
+// ...existing layout code or children rendering...
+
+export default function AdminLayout({ children }) {
+  return children;
+}
